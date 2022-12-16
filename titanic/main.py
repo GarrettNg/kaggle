0cd06312f5b9869f5f5f058ca28c85c4b39fbfc0
Garrett Ng <56460265+GarrettNg@users.noreply.github.com>
2022-12-16 18:22:14 -0500

    Add some exploratory analysis

--- titanic/main.py
+++ titanic/main.py
@@ -12,6 +12,33 @@ TEST_DATA_PATH = "./data/test.csv"
 TRAINING_DATA_PATH = "./data/train.csv"
 
 
+def check_input(input_files: list) -> None:
+    """Print head of input csv files
+    :param input_files: list of input files
+    """
+    dfs = [pd.read_csv(f) for f in input_files]
+    [print(df.head()) for df in dfs]
+
+
+def get_feature_rate(
+    dataset: pd.DataFrame, feature_header: str, feature_value: str, feature_outcome: str
+) -> float:
+    """Get survival rate for a given feature
+    :param dataset: DataFrame containing input data
+    :param feature_header: feature header from data set
+    :param feature_value: feature value from data set
+    :param feature_outcome: feature outcome from data set
+    :return: feature rate
+    """
+    feature = dataset.loc[dataset[feature_header] == feature_value][feature_outcome]
+    rate = sum(feature) / len(feature)
+    print(
+        f"% of '{feature_value}' ('{feature_header}') with outcome of '{feature_outcome}':",
+        rate,
+    )
+    return rate
+
+
 def train_predict(
     train_data: pd.DataFrame,
     test_data: pd.DataFrame,
@@ -39,6 +66,13 @@ def train_predict(
 
 
 if __name__ == "__main__":
+    # check_input([TEST_DATA_PATH, TRAINING_DATA_PATH])
+
     train_data = pd.read_csv(TRAINING_DATA_PATH)
     test_data = pd.read_csv(TEST_DATA_PATH)
+
+    # Survival rate by sex
+    # female_survival_rate = get_feature_rate(train_data, "Sex", "female", "Survived")
+    # male_survival_rate = get_feature_rate(train_data, "Sex", "male", "Survived")
+
     train_predict(train_data, test_data)
